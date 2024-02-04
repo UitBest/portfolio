8b64e4e39e43088d7941024d17461856838f6b6f
Timo Mac <82875662+TimoCuijpers@users.noreply.github.com>
2024-02-04 22:15:02 +0100

    Meta toegevoegd

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="api-host" content="{API_HOST}">
 
+    <title>Timo Cuijpers | Portfolio</title>
+
+    <!-- Beschrijving van de pagina -->
+    <meta name="description" content="Ontdek het portfolio van Timo Cuijpers. Blijf op de hoogte voor meer informatie en updates over creatieve ontwerpen en projecten.">
+
+    <!-- Trefwoorden voor de pagina -->
+    <meta name="keywords" content="Timo Cuijpers, Portfolio, Webdesign, Creatief Ontwerp, Grafisch Design">
+
+    <!-- Open Graph / Facebook Meta Tags -->
+    <meta property="og:title" content="Timo Cuijpers | Portfolio">
+    <meta property="og:description" content="Ontdek het portfolio van Timo Cuijpers. Blijf op de hoogte voor meer informatie en updates over creatieve ontwerpen en projecten.">
+    <meta property="og:image" content="/img/MainBackground.jpg">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://timo.uit.best">
+
+    <!-- Twitter Card Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Timo Cuijpers | Portfolio">
+    <meta name="twitter:description" content="Ontdek het portfolio van Timo Cuijpers. Blijf op de hoogte voor meer informatie en updates over creatieve ontwerpen en projecten.">
+    <meta name="twitter:image" content="/img/MainBackground.jpg">
+
     {{--  Styles  --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
 
@@ -13,9 +34,6 @@
 
     {{--  Scripts  --}}
     @vite('resources/css/app.css')
-
-    {{--  Title  --}}
-    <title>Timo Cuijpers</title>
 
 </head>
 <body>
