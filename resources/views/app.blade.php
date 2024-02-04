28683f4632c5566b94cf79523ff75532b2c5ef7d
Timo Mac <82875662+TimoCuijpers@users.noreply.github.com>
2024-02-04 22:40:34 +0100

    Update meta image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,7 +17,7 @@
     <!-- Open Graph / Facebook Meta Tags -->
     <meta property="og:title" content="Timo Cuijpers | Portfolio">
     <meta property="og:description" content="Ontdek het portfolio van Timo Cuijpers. Blijf op de hoogte voor meer informatie en updates over creatieve ontwerpen en projecten.">
-    <meta property="og:image" content="/img/TimoCutOut.png">
+    <meta property="og:image" content="https://timo.uit.best/img/Timo.jpeg">
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://timo.uit.best">
 
@@ -25,7 +25,7 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Timo Cuijpers | Portfolio">
     <meta name="twitter:description" content="Ontdek het portfolio van Timo Cuijpers. Blijf op de hoogte voor meer informatie en updates over creatieve ontwerpen en projecten.">
-    <meta name="twitter:image" content="/img/TimoCutOut.png">
+    <meta name="twitter:image" content="https://timo.uit.best/img/Timo.jpeg">
 
     {{--  Styles  --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
