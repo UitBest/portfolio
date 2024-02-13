8ef0453c51a2994df2014e2d74771f5c5f08000b
Timo Mac <82875662+TimoCuijpers@users.noreply.github.com>
2024-02-13 11:55:32 +0100

    Update Meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,18 @@
 
     <title>Timo Cuijpers | Portfolio</title>
 
+    <!-- Meta tag voor responsive design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="color-scheme" content="dark" />
+
     <!-- Beschrijving van de pagina -->
     <meta name="description" content="Ontdek het portfolio van Timo Cuijpers. Blijf op de hoogte voor meer informatie en updates over creatieve ontwerpen en projecten.">
 
     <!-- Trefwoorden voor de pagina -->
-    <meta name="keywords" content="Timo Cuijpers, Portfolio, Webdesign, Creatief Ontwerp, Grafisch Design">
+    <meta name="keywords" content="Timo Cuijpers, Portfolio, Portfolio Timo Cuijpers, Portfolio Timo, Timo Portfolio, Timo Cuijpers Portfolio, Webdesign, Webdevelopment">
 
     <!-- Open Graph / Facebook Meta Tags -->
+    <meta property="og:site_name" content="Timo Cuijpers" />
     <meta property="og:title" content="Timo Cuijpers | Portfolio">
     <meta property="og:description" content="Ontdek het portfolio van Timo Cuijpers. Blijf op de hoogte voor meer informatie en updates over creatieve ontwerpen en projecten.">
     <meta property="og:image" content="https://timo.uit.best/img/Timo.jpeg">
@@ -26,10 +31,10 @@
     <meta name="twitter:title" content="Timo Cuijpers | Portfolio">
     <meta name="twitter:description" content="Ontdek het portfolio van Timo Cuijpers. Blijf op de hoogte voor meer informatie en updates over creatieve ontwerpen en projecten.">
     <meta name="twitter:image" content="https://timo.uit.best/img/Timo.jpeg">
+    <meta name="twitter:maxage" content="86400" />
 
     {{--  Styles  --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
-
     <link rel="icon" href="/img/Timo.jpeg" />
 
     {{--  Scripts  --}}
