58fa39c1de011df75c76d0a0e2b98c361ad0adf9
Timo Cuijpers <timo@uit.best>
2024-05-30 19:49:33 +0200

    Mooi

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -42,64 +42,69 @@
                     :width="mobile ? '100%' : '50%'"
                 >
                     <v-card
+                        class="d-flex align-center position-relative justify-center"
+                        :class="showExtraIntroInfo && 'pb-16'"
                         color="transparent"
                         elevation="24"
+                        :min-height="!mobile ? 300 : undefined"
                         style="backdrop-filter: blur(20px)"
                     >
-                        <v-card-title class="text-primary"> Bio </v-card-title>
-                        <v-card-text class="pb-0">
-                            <div class="text-caption text-primary">OVER MIJ</div>
-                            <div>Welkom op mijn portfolio website.</div>
-                            <br />
-                            <div>Ik ben Timo Cuijpers, geboren en getogen in Best.</div>
-                            <br />
-                            <div>
-                                Tijdens mijn afstudeerstage bij "WEAP" ontstond er een enorme persoonlijke en
-                                professionele groei. Deze ontwikkeling was te danken aan de professionele werkomgeving en
-                                het aanleren van verantwoordelijkheidsgevoel.
-                            </div>
-                            <br />
+                        <v-sheet>
+                            <v-card-title class="text-primary"> Bio </v-card-title>
+                            <v-card-text class="pb-0">
+                                <div class="text-caption text-primary">OVER MIJ</div>
+                                <div>Welkom op mijn portfolio website.</div>
+                                <br />
+                                <div>Ik ben Timo Cuijpers, geboren en getogen in Best.</div>
+                                <br />
+                                <div>
+                                    Tijdens mijn afstudeerstage bij "WEAP" ontstond er een enorme persoonlijke en
+                                    professionele groei. Deze ontwikkeling was te danken aan de professionele
+                                    werkomgeving en het aanleren van verantwoordelijkheidsgevoel.
+                                </div>
+                                <br />
 
-                            <v-expand-transition>
-                                <v-sheet v-show="showExtraIntroInfo">
-                                    <v-divider /><br />
-                                    <div class="text-caption text-primary">STUDIE</div>
-                                    <div>
-                                        In 2020 vond er een overgang plaats van het Kempenhorst College in Oirschot naar
-                                        het Summa College in Eindhoven. Aanvankelijk was de inschrijving voor de
-                                        opleiding Expert IT, maar binnen een half jaar werd duidelijk dat een grotere
-                                        interesse lag bij programmeren.
-                                    </div>
-                                    <br />
-                                    <div>
-                                        Deze nieuwe inzichten leidden tot een overstap naar de opleiding 'Software
-                                        Developer', die begin 2024 succesvol werd afgerond.
-                                    </div>
-                                    <br />
-                                    <v-divider /><br />
-                                    <div class="text-caption text-primary">EIGEN BEDRIJF</div>
-                                    <div>
-                                        Na het versneld afronden van de opleiding en een leerzame stageperiode, werd de
-                                        stap gezet om een eigen onderneming, 'Uit Best', op te richten. Deze kans deed
-                                        zich voor in een kamer van
-                                        <a
-                                            class="text-white"
-                                            href="https://www.dieder.it/"
-                                            target="_blank"
-                                            >mijn vaders bedrijf</a
-                                        >. Dankbaarheid wordt uitgesproken voor de mogelijkheid en de waardevolle
-                                        adviezen over het runnen van een eigen bedrijf.
-                                    </div>
-                                    <br />
-                                    <div>
-                                        De eindstage bood essentiële inzichten en het vertrouwen dat het starten van een
-                                        eigen bedrijf een haalbare droom is.
-                                    </div>
-                                </v-sheet>
-                            </v-expand-transition>
-                        </v-card-text>
+                                <v-expand-transition>
+                                    <v-sheet v-show="showExtraIntroInfo">
+                                        <v-divider /><br />
+                                        <div class="text-caption text-primary">STUDIE</div>
+                                        <div>
+                                            In 2020 vond er een overgang plaats van het Kempenhorst College in Oirschot
+                                            naar het Summa College in Eindhoven. Aanvankelijk was de inschrijving voor de
+                                            opleiding Expert IT, maar binnen een half jaar werd duidelijk dat een grotere
+                                            interesse lag bij programmeren.
+                                        </div>
+                                        <br />
+                                        <div>
+                                            Deze nieuwe inzichten leidden tot een overstap naar de opleiding 'Software
+                                            Developer', die begin 2024 succesvol werd afgerond.
+                                        </div>
+                                        <br />
+                                        <v-divider /><br />
+                                        <div class="text-caption text-primary">EIGEN BEDRIJF</div>
+                                        <div>
+                                            Na het versneld afronden van de opleiding en een leerzame stageperiode, werd
+                                            de stap gezet om een eigen onderneming, 'Uit Best', op te richten. Deze kans
+                                            deed zich voor in een kamer van
+                                            <a
+                                                class="text-white"
+                                                href="https://www.dieder.it/"
+                                                target="_blank"
+                                                >mijn vaders bedrijf</a
+                                            >. Dankbaarheid wordt uitgesproken voor de mogelijkheid en de waardevolle
+                                            adviezen over het runnen van een eigen bedrijf.
+                                        </div>
+                                        <br />
+                                        <div>
+                                            De eindstage bood essentiële inzichten en het vertrouwen dat het starten van
+                                            een eigen bedrijf een haalbare droom is.
+                                        </div>
+                                    </v-sheet>
+                                </v-expand-transition>
+                            </v-card-text>
+                        </v-sheet>
 
-                        <v-card-actions class="justify-center">
+                        <v-card-actions class="position-absolute bottom-0">
                             <v-btn
                                 :icon="(showExtraIntroInfo ? 'mdi-rotate-180' : '') + ' mdi-chevron-down'"
                                 variant="text"
@@ -114,27 +119,33 @@
                     :width="mobile ? '100%' : '50%'"
                 >
                     <v-card
+                        class="d-flex align-center position-relative justify-center"
                         color="transparent"
                         elevation="24"
                         height="100%"
+                        :min-height="!mobile ? 300 : undefined"
                         style="backdrop-filter: blur(20px)"
                     >
-                        <v-card-title class="text-primary"> Hobby's </v-card-title>
-                        <v-card-text>
-                            <div class="text-caption text-primary">GITAAR</div>
-                            <span
-                                >Tijdens de stage bij "WEAP" ontstond de gelegenheid om een gitaar te lenen. Dit leidde
-                                tot een groeiende interesse in het spelen van simpele akoestische nummers.</span
-                            ><br /><br />
+                        <v-sheet>
+                            <v-card-title class="text-primary"> Hobby's </v-card-title>
+                            <v-card-text>
+                                <div class="text-caption text-primary">GITAAR</div>
+                                <span>
+                                    Tijdens de stage bij "WEAP" ontstond de gelegenheid om een gitaar te lenen. Dit
+                                    leidde tot een groeiende interesse in het spelen van simpele akoestische nummers.
+                                </span>
+                                <br />
+                                <br />
 
-                            <div class="text-caption text-primary">BASKETBAL</div>
-                            <span
-                                >Basketbal, gestart in 2016, is uitgegroeid tot een belangrijke activiteit, met BCO
-                                Oirschot als de gekozen club voor training en wedstrijden. Deze sport biedt niet alleen
-                                een manier om fysiek actief te zijn, maar ook een platform voor persoonlijke
-                                ontwikkeling.</span
-                            >
-                        </v-card-text>
+                                <div class="text-caption text-primary">BASKETBAL</div>
+                                <span>
+                                    Basketbal, gestart in 2016, is uitgegroeid tot een belangrijke activiteit, met BCO
+                                    Oirschot als de gekozen club voor training en wedstrijden. Deze sport biedt niet
+                                    alleen een manier om fysiek actief te zijn, maar ook een platform voor persoonlijke
+                                    ontwikkeling.
+                                </span>
+                            </v-card-text>
+                        </v-sheet>
                     </v-card>
                 </v-sheet>
             </v-sheet>
